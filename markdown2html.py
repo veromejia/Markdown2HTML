284f5257cf1e63a247a0c763b1dced7e5bf1b0ce
veromejia <veromejia.q@gmail.com>
2020-07-29 21:18:58 -0700

    deleting changes

--- markdown2html.py
+++ markdown2html.py
@@ -21,14 +21,13 @@ if __name__ == '__main__':
         print("Missing {}".format(my_mdfile), file=sys.stderr)
         exit(1)
 
-    with open(my_mdfile, 'r') as mdfile:
+    """with open(my_mdfile, 'r') as mdfile:
         with open(sys.argv[2], 'w') as htmlfile:
             for line in mdfile:
                 headings = line.split(" ", 1)[0]
                 new_line = (line.split(" ", 1)[1]).rstrip('\n')
-                count = len(headings)
                 for key, val in my_dict.items():
                     if headings == key:
                         htmlfile.write(
-                            "<{}>{}</{}>\n".format(val, new_line, val))
+                            "<{}>{}</{}>\n".format(val, new_line, val))"""
     exit(0)
