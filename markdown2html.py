ff2e35596c1993d7f254938c1652886fca6d865b
veromejia <veromejia.q@gmail.com>
2020-07-29 21:05:52 -0700

    making changes

--- markdown2html.py
+++ markdown2html.py
@@ -8,26 +8,27 @@ import sys
 import os.path
 
 if __name__ == '__main__':
+    my_dict = {"#": "h1", "##": "h2", "###": "h3",
+               "####": "h4", "#####": "h5", "######": "h6"}
     if (len(sys.argv) <= 2):
         print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
         exit(1)
 
     my_mdfile = sys.argv[1]
-    my_dict = {"#": "h1", "##": "h2", "###": "h3",
-               "####": "h4", "#####": "h5", "######": "h6"}
-
     name, exten = os.path.splitext(my_mdfile)
     if (not os.path.exists(my_mdfile) or not exten == ".md"):
         print("Missing {}".format(my_mdfile), file=sys.stderr)
         exit(1)
 
-    with open(my_mdfile, 'r') as mdfile, open(sys.argv[2], 'w') as htmlfile:
-        for line in mdfile:
-            headings = line.split(" ", 1)[0]
-            new_line = (line.split(" ", 1)[1]).rstrip('\n')
-            count = len(headings)
-            for key, val in my_dict.items():
-                if headings == key:
-                    htmlfile.write("<{}>{}</{}>\n".format(val, new_line, val))
-        exit(0)
+    with open(my_mdfile, 'r') as mdfile:
+        with open(sys.argv[2], 'w') as htmlfile:
+            for line in mdfile:
+                headings = line.split(" ", 1)[0]
+                new_line = (line.split(" ", 1)[1]).rstrip('\n')
+                count = len(headings)
+                for key, val in my_dict.items():
+                    if headings == key:
+                        htmlfile.write(
+                            "<{}>{}</{}>\n".format(val, new_line, val))
+    exit(0)
